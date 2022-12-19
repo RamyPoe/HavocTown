9bff80cbe55d20bea37bd0daace9a2849a95aae3
RamyPoe <coolshifter99@gmail.com>
2022-12-18 21:20:00 -0500

    Menu Buttons

--- core/src/com/project/game/Scenes/MainMenuHud.java
+++ core/src/com/project/game/Scenes/MainMenuHud.java
@@ -4,13 +4,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Cursor.SystemCursor;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -39,34 +46,58 @@ public class MainMenuHud implements Disposable {
         // Background image
         backgroundTexture = new Texture(Gdx.files.internal("maps/1.jpg"));
 
-        // Font and Label
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/menu-font.ttf"));
-        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
 
-        parameter.size = 96;
-        parameter.color = Color.WHITE;
-        parameter.borderWidth = 2f;
-        parameter.borderColor = Color.BLACK;
-        
-        BitmapFont bFont = generator.generateFont(parameter); 
-        generator.dispose();
-
+        // Label
+        /*
         Label.LabelStyle style = new Label.LabelStyle(bFont, Color.WHITE);
         Label lbl = new Label("HELLO WORLD!", style);
+        */
+
+        // Setup for buttons
+        MenuButton.loadButtonFont("fonts/menu-font");
+
+        // Buttons
+        MenuButton campaignGameButton = new MenuButton("CAMPAIGN", "story mode");
+        MenuButton customGameButton = new MenuButton("CUSTOM GAME", "custom game");
+
+
+        // Button Callbacks
+        campaignGameButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("CAMPAIGN CLICKED!");
+            }
+        });
+        
+
 
         // Table
         Table table = new Table();
         table.setFillParent(true);
-        table.top();
-        table.add(lbl).expandX();
+        table.top().right();
+        table.padRight(20);
+        table.padTop(20);
+
+        // Spacing from top
+        table.row().padTop(200);
+
+        // Add buttons to table
+        table.add(campaignGameButton).align(Align.right);
+        table.row();
+        table.add(customGameButton).align(Align.right);
 
         // Add table to stage
         stage.addActor(table);
 
-        
+        // So that clicks work
+        Gdx.input.setInputProcessor(stage);
+
     }
 
     public void draw(float delta) {
+
+        // For animations
+        stage.act();
 
         // Get batch ready
         stage.getBatch().setProjectionMatrix(stage.getCamera().combined);
@@ -78,11 +109,13 @@ public class MainMenuHud implements Disposable {
         
         // Draw GUI
         stage.draw();
+
     }
 
     @Override
     public void dispose() {
         stage.dispose();
+        MenuButton.dispose();
     }
 
 }
---
+++ core/src/com/project/game/Scenes/MenuButton.java
@@ -0,0 +1,95 @@
+package com.project.game.Scenes;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Cursor.SystemCursor;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Disposable;
+
+public class MenuButton extends TextButton {
+
+    private String text, description;
+    private static BitmapFont buttonFont, descriptionFont;
+    private static TextButtonStyle txtBtnStyle; 
+
+    public MenuButton(String text, String description) {
+
+        // Super
+        super(text, txtBtnStyle);
+
+        // Text specific to this button
+        this.text = text;
+        this.description = description;
+
+        // For mouse cursor changing
+        addEventListener();
+
+    }
+
+    public void addEventListener() {
+
+        addListener(new ClickListener() {
+
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                Gdx.graphics.setSystemCursor(SystemCursor.Hand);
+            }
+
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                Gdx.graphics.setSystemCursor(SystemCursor.Arrow);
+            }
+
+        });
+
+    }
+
+    public static void loadButtonFont(String path) {
+        // File handle
+        FileHandle fontFileHandle = Gdx.files.internal("fonts/menu-font.ttf");
+
+        // Load Fonts
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(fontFileHandle);
+        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+
+        // Set parameters
+        parameter.size = 96;
+        parameter.color = Color.WHITE;
+        parameter.borderWidth = 2f;
+        parameter.borderColor = Color.BLACK;
+        
+        // Save font
+        buttonFont = generator.generateFont(parameter); 
+        
+        // Set parameters
+        parameter.size = 32;
+        parameter.color = Color.WHITE;
+        parameter.borderWidth = 0;
+        parameter.borderColor = Color.WHITE;
+        
+        // Save font
+        descriptionFont = generator.generateFont(parameter); 
+
+        // De-allocate
+        parameter = null;
+        generator.dispose();
+        generator = null;
+
+        // Button style
+        txtBtnStyle = new TextButtonStyle();
+        txtBtnStyle.font = buttonFont;
+    }
+
+    public static void dispose() {
+        buttonFont.dispose();
+        descriptionFont.dispose();
+    }
+
+}
